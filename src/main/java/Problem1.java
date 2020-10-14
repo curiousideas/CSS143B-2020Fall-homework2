421d315781216210095025e81f7727955de8625c
Dani <shaykhodani@gmail.com>
2020-10-13 18:33:27 -0700

    Finished problem 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,10 +1,36 @@
 public class Problem1 {
 
     /*
-    Describe your algorithm here in up to 5 sentences:
+    I have 3 pointers. Start indicates where the 0's should be, an index pointer that moves to each index, and
+    a end pointer indicating where the 2's go. If nums[index] is 0 it is sent to nums[start]. If nums[index] is 1 its skipped.
+    If nums[index] is 2 its sent to nums[end].
      */
 
     // Do not change signature (function name, parameters type, and return type)
     public static void colorSort(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) {
+            return;
+        }
+
+        int start = 0; // 2
+        int end = nums.length - 1; // 5
+        int index = 0; // 4
+        // {1,1,0,1,2,0,1,2,1},
+        while (index <= end && start < end) {
+            if (nums[index] == 0) {
+                nums[index] = nums[start];
+                nums[start] = 0;
+                start++;
+                index++;
+            } else if (nums[index] == 2) {
+                nums[index] = nums[end];
+                nums[end] = 2;
+                end--;
+            } else {
+                index++;
+            }
+        }
     }
 }
+
+
