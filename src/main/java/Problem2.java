015413033987501ed11b6d81f953927db842d385
Dani <shaykhodani@gmail.com>
2020-10-14 22:08:10 -0700

    Finished problem 2

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -7,6 +7,16 @@ public class Problem2 {
     // Do not change signature (function name, parameters and return type)
     // Do not use extra space for another array. Solution to be in-pace with O(1) extra memory
     public static int remove(int[] nums) {
-        return Integer.MAX_VALUE; // place holder
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+// {0, 0, 1, 1, 1, 2, 2, 4, 4, 6},
+        int index = 1; //2
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i+1]) {
+                nums[index++] = nums[i+1];
+            }
+        }
+        return index;
     }
 }
